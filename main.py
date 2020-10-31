cf7837ebfee54f65c597a2dba0dae05c326e6d37
thek1d <oezguer.guemueslue@gmail.com>
2020-10-31 15:29:04 +0100

    refactoring

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ Created on Oct 31, 2020
 '''
 
 import json, math
+from Ellipsoid.ellipsoid_calculator import Plotter
 
 class Config():
             
     @staticmethod
     def initDataStucture():
         return{
-                    'x0'         : 0,
-                    'y0'         : 0,
-                    'total_steps': 0,
-                    'theta0'     : math.radians(0),
-                    'rotation'   : math.radians(0),
-                    'covariance' : 
+                    'x0'                    : 0,
+                    'y0'                    : 0,
+                    'step_size'             : 0,
+                    'movement_size_1'       : 0,
+                    'movement_size_2'       : 0,
+                    'movement_size_rotation': 0,
+                    'total_steps'           : 0,
+                    'theta0'                : math.radians(0),
+                    'rotation'              : math.radians(0),
+                    'covariance'            : 
                     {
                         'wheel_left' : 0,
                         'wheel_right': 0
@@ -29,11 +34,15 @@ class Config():
         data = file.read()
         config = json.loads(data)
         
-        dataset['x0']          = config['x0']
-        dataset['y0']          = config['y0']
-        dataset['total_steps'] = config['total_steps']
-        dataset['theta0']      = math.radians(config['theta0'])
-        dataset['rotation']    = math.radians(config['rotation'])
+        dataset['x0']                     = config['x0']
+        dataset['y0']                     = config['y0']
+        dataset['step_size']              = config['step_size']
+        dataset['movement_size_1']        = config['movement_size_1']
+        dataset['movement_size_2']        = config['movement_size_2']
+        dataset['movement_size_rotation'] = config['movement_size_rotation']
+        dataset['total_steps']            = dataset['movement_size_1'] + dataset['movement_size_2']
+        dataset['theta0']                 = math.radians(config['theta0'])
+        dataset['rotation']               = math.radians(config['rotation'])
                 
         covariance = config['covarianz']
         
@@ -42,10 +51,15 @@ class Config():
             dataset['covariance']['wheel_right'] = covariance[elem].get('wheel_right')
         
         
+        
 if __name__ == '__main__':
     
     dataset = Config.initDataStucture()
     Config.readJson(path2JsonFile="config.json", dataset=dataset)
+    
+    pt = Plotter()
+    pt.plotMeanPoints(dataset=dataset)
+    
     
     
     
