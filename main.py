48dc3d072afc65c75504f1f084486b6db387eed8
thek1d <oezguer.guemueslue@gmail.com>
2020-10-31 11:34:00 +0100

    added config file, init datastructure

--- main.py
+++ main.py
@@ -4,5 +4,49 @@ Created on Oct 31, 2020
 @author: thek1d
 '''
 
+import json, math
+
+class Config():
+            
+    @staticmethod
+    def initDataStucture():
+        return{
+                    'x0'         : 0,
+                    'y0'         : 0,
+                    'total_steps': 0,
+                    'theta0'     : math.radians(0),
+                    'rotation'   : math.radians(0),
+                    'covariance' : 
+                    {
+                        'wheel_left' : 0,
+                        'wheel_right': 0
+                    }
+              }
+    
+    @staticmethod
+    def readJson(path2JsonFile, dataset):
+        file = open(path2JsonFile, 'r')
+        data = file.read()
+        config = json.loads(data)
+        
+        dataset['x0']          = config['x0']
+        dataset['y0']          = config['y0']
+        dataset['total_steps'] = config['total_steps']
+        dataset['theta0']      = math.radians(config['theta0'])
+        dataset['rotation']    = math.radians(config['rotation'])
+                
+        covariance = config['covarianz']
+        
+        for elem in range(len(covariance)):
+            dataset['covariance']['wheel_left']  = covariance[elem].get('wheel_left')
+            dataset['covariance']['wheel_right'] = covariance[elem].get('wheel_right')
+        
+        
 if __name__ == '__main__':
-    pass
+    
+    dataset = Config.initDataStucture()
+    Config.readJson(path2JsonFile="config.json", dataset=dataset)
+    
+    
+    
+    
